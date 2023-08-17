04e353629158cdf822e2c0f6951c49c6efe1ab54
Ken-Nguyen-13 <ken.ak.nguyen@gmail.com>
2023-08-17 19:57:50 -0400

    Added logic to capitalize word inputs

--- script.js
+++ script.js
@@ -10,25 +10,36 @@ insertBtn.addEventListener("click", function () {
   inputWordsLabel.classList.remove("hidden");
 });
 
+function allLetter(inputText) {
+  const letters = /^[A-Za-z]+$/; // Must include one or more character between a-z, lowercase or uppercase
+  if (inputText.value.match(letters)) {
+    return true;
+  } else {
+    alert(`Invalid characters.`);
+    return false;
+  }
+}
 document.addEventListener("keydown", function (e) {
-  if (e.key === "Enter" && !inputWords.classList.contains("hidden") && inputWords.value) {
+  if (
+    e.key === "Enter" && !inputWords.classList.contains("hidden") && allLetter(inputWords)) {
     alphabetizeBtn.classList.remove("hidden");
-    numOfWords++
+    numOfWords++;
     const word = document.createElement("li");
-    word.id = `word${numOfWords}`
+    word.id = `word${numOfWords}`;
+    inputWords.value = inputWords.value.charAt(0).toUpperCase() + inputWords.value.slice(1);
     word.textContent = inputWords.value;
     word.style.fontSize = "50px";
     document.querySelector(".word-list").appendChild(word);
     inputWords.value = "";
   }
 });
-alphabetizeBtn.addEventListener('click', function () {
+alphabetizeBtn.addEventListener("click", function () {
   let arrOfWords = [];
   for (let i = 1; i <= numOfWords; i++) {
-    arrOfWords.push(document.getElementById(`word${i}`).textContent)
+    arrOfWords.push(document.getElementById(`word${i}`).textContent);
   }
-  console.log(arrOfWords.sort())
+  console.log(arrOfWords.sort());
   for (let i = 1; i <= numOfWords; i++) {
-    document.getElementById(`word${i}`).textContent = arrOfWords[(i - 1)];
+    document.getElementById(`word${i}`).textContent = arrOfWords[i - 1];
   }
-})
+});
